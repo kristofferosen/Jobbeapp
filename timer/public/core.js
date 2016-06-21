f0e09126d45ad2d276e1d9f3796cc14759fb50c7
Kristoffer Gard Osen <kristofferosen@gmail.com>
2016-06-21 21:13:47 +0200

    done

--- timer/public/core.js
+++ timer/public/core.js
@@ -5,12 +5,27 @@ var jobbeapp = angular.module('jobbeapp', []);
 
 function mainController($scope, $http) {
     $scope.formData = {};
+    $scope.kristofferTotal = 0;
+    $scope.christerTotal = 0;
+    $scope.danielTotal = 0;
+    $scope.timertotalt = 0;
 
     // when landing on the page, get all todos and show them
     $http.get('/api/jobbs')
         .success(function(data) {
             $scope.jobbs = data;
             console.log(data);
+            data.forEach(function(jobb){
+
+                $scope.timertotalt = $scope.timertotalt + jobb.Timer;
+
+                if(jobb.Jobbemann == "Kristoffer")
+                    $scope.kristofferTotal = $scope.kristofferTotal + jobb.Timer;
+                else if(jobb.Jobbemann == "Christer")
+                    $scope.christerTotal = $scope.christerTotal + jobb.Timer;
+                else if(jobb.Jobbemann == "Daniel")
+                    $scope.danielTotal = $scope.danielTotal + jobb.Timer;
+            });
         })
         .error(function(data) {
             console.log('Error: ' + data);
